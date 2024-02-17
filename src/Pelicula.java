a35dffcb2a3e1e88271010cdb98aa2a98217e57e
unknown <yiyiliman113@gmail.com>
2024-02-17 22:06:05 +0100

    (doc) comentario de la clase pelicula

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,28 +1,42 @@
-
+/**
+ * nombre de la clase
+ */
 public class Pelicula {
-
+    /**
+     * los atributos de la clase
+     */
     private String titulo;
     private int duracion;
 
-
+    /**
+     *El constructor
+     */
     public Pelicula(String titulo, int duracion) {
         this.titulo = titulo;
         this.duracion = duracion;
     }
 
-
+    /**
+     *getTitulo obtiene el valor de un atributo
+     */
     public String getTitulo() {
         return titulo;
     }
-
+    /**
+     *setTitulo establece el valor al atributo
+     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
+    /**
+     *getDuracion obtiene el valor de un atributo
+     */
     public int getDuracion() {
         return duracion;
     }
-
+    /**
+     *setDuracion establece el valor al atributo
+     */
     public void setDuracion(int duracion) {
         this.duracion = duracion;
     }
