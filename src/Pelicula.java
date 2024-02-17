54fd069e1a524658798844ef8c14b5c8d4874242
unknown <yiyiliman113@gmail.com>
2024-02-17 22:04:15 +0100

    (core creacion del objeto pelicula)

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,31 @@
+
 public class Pelicula {
+
+    private String titulo;
+    private int duracion;
+
+
+    public Pelicula(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+
 }
