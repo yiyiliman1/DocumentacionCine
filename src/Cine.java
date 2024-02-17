d008f353b2a15284192e25bd842d09fc7af14fb4
unknown <yiyiliman113@gmail.com>
2024-02-17 21:50:47 +0100

    (doc) comentario de la clase cine

--- src/Cine.java
+++ src/Cine.java
@@ -1,36 +1,57 @@
+/**
+ * nombre de la clase
+ */
 
 public class Cine {
-
+    /**
+     * los atributos
+     */
     private String nombre;
     private int aforo = 20;
     private int sala;
 
+    /**
+     *El constructor
+     */
     public Cine(String nombre, int aforo, int sala) {
         this.nombre = nombre;
         this.aforo = aforo;
         this.sala = sala;
     }
 
+    /**
+     *getNombre obtiene el valor de un atributo
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     *setNombre establece el valor al atributo
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     *getAforo obtiene el valor de un atributo
+     */
     public int getAforo() {
         return aforo;
     }
-
+    /**
+     *setAforo establece el valor al atributo
+     */
     public void setAforo(int aforo) {
         this.aforo = aforo;
     }
-
+    /**
+     *getSala obtiene el valor de un atributo
+     */
     public int getSala() {
         return sala;
     }
-
+    /**
+     *setSala establece el valor al atributo
+     */
     public void setSala(int sala) {
         this.sala = sala;
     }
